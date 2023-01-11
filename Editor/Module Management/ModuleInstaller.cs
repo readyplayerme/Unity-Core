8b201ffe0bec4134a870aaeef80466cbb424c295
Harrison <harrisonhough4@gmail.com>
2023-01-11 11:15:40 +0200

    test: reorder operations

--- Editor/Module Management/ModuleInstaller.cs
+++ Editor/Module Management/ModuleInstaller.cs
@@ -181,8 +181,8 @@ namespace ReadyPlayerMe.Core.Editor
             if (allModuleInstalled)
             {
                 SDKLogger.Log(TAG, MODULE_INSTALLATION_SUCCESS_MESSAGE);
-                CompilationPipeline.RequestScriptCompilation();
                 AssetDatabase.Refresh();
+                CompilationPipeline.RequestScriptCompilation();
             }
             else
             {
